a88003a764f6e36ddbe2f21793a43065ff69c31d
Kepler-17c <florian.gilges@gmx.de>
2017-12-18 16:26:32 +0100

    Improved class description

--- src/edu/kit/ukzgk/interval/number/Number.java
+++ src/edu/kit/ukzgk/interval/number/Number.java
@@ -7,28 +7,39 @@ package edu.kit.ukzgk.interval.number;
  * \mathbb{K} U+1D542    &#120130;
  */
 /**
- * For any objects, that can be used in calculative manner, this interface provides all functions available on an
- * algebraic field. In addition some functions are added to give access to basic properties.<br>
+ * For any objects, that can be used in calculative manner, this interface provides all functions available on an <a
+ * href="https://en.wikipedia.org/wiki/Field_(mathematics)">algebraic field</a>. In addition some functions are added to
+ * give access to basic properties.<br>
  * <br>
- * Operations defined on fields are:<br>
+ * Here is, in short, the definition of a field, from bottom to top:<br>
+ * A composition is a transformation, that assigns one element of a set to two other elements of the same set.<br>
+ * A group is a pair of a non-empty set S and a composition &#9913; on S with the following properties:<br>
  * <table>
  * <tr>
- * <th>Operation</th>
- * <th>Property</th>
+ * <td>G1</td>
+ * <td>Associativity</td>
+ * <td>&forall; a,b,c &isin; S : (a &#9913; b) &#9913; c = a &#9913; (b &#9913; c)</td>
  * </tr>
  * <tr>
- * <td>a + b = c</td>
- * <td>Adding two elements yield to another element in the field</td>
+ * <td>G2</td>
+ * <td>Neutral element</td>
+ * <td>&exist; e &isin; S &forall; a &isin; S : e &#9913; a = a = a &#9913; e</td>
  * </tr>
  * <tr>
- * <td>a * b = c</td>
- * <td>Multiplying two elements</td>
+ * <td>G3</td>
+ * <td>Inverse element</td>
+ * <td>&forall; a &isin; S &exist; a<sup>-1</sup> &isin; S : a<sup>-1</sup> &#9913; a = e = a &#9913; a<sup>-1</sup></td>
  * </tr>
  * <tr>
- * <td rowspan="2">For a field &#120130;<br>
- * a,b,c &#8712; &#120130;</td>
+ * <td colspan="3">Groups with this last property additionally are called abelian groups:</td>
+ * </tr>
+ * <tr>
+ * <td>G4</td>
+ * <td>Commutativity</td>
+ * <td>&forall; a,b &isin; S : a &#9913; b = b &#9913; a</td>
  * </tr>
  * </table>
+ * TODO: Group -> Ring -> Field
  *
  * @param <T>
  */
